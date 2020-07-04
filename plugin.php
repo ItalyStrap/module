3760fa7eaa75ace1e8f54e7a4f2a74280942080b
Enea <info@overclokk.net>
2020-07-04 09:05:09 +0200

    First commit

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Module
+ * Description:
+ * Plugin URI: https://italystrap.com
+ * Author: Enea Overclokk
+ * Author URI: https://italystrap.com
+ * Version: 0.0.1
+ * License: MIT
+ * Text Domain: italystrap
+ * Domain Path: languages
+ */
